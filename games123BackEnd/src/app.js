384bada04bf01b2a89ac38af578e6fba971d50c2
barathviknesh <barathviknesh.9566@gmail.com>
2025-02-17 22:51:47 +0530

    razorpay backend implementation init

--- games123BackEnd/src/app.js
+++ games123BackEnd/src/app.js
@@ -3,16 +3,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const authRoutes = require("./routes/authRoutes");
-const gameRoutes = require("./routes/gameRoutes");
-const walletRoutes = require("./routes/walletRoutes");
+// const authRoutes = require("./routes/authRoutes");
+// const gameRoutes = require("./routes/gameRoutes");
+// const walletRoutes = require("./routes/walletRoutes");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/games", gameRoutes);
-app.use("/api/wallet", walletRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/games", gameRoutes);
+// app.use("/api/wallet", walletRoutes);
 
 module.exports = app;
