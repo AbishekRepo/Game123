5a4966c39e2246b0b762a78b9e5869c690c69f87
Abishek <abishekhttps@gmail.com>
2025-02-17 08:43:41 +0530

    Revert "razorpay single run test!"
    This reverts commit 1a4335f77d47f2dcee7b205606283f9891c46641.

--- components/ui/shared/Header/Wallet-Balance/index.tsx
+++ components/ui/shared/Header/Wallet-Balance/index.tsx
@@ -22,58 +22,6 @@ const WalletBalance = ({
     fetchBalance();
   }, [fetchBalance]);
 
-  // Test code dont touch this file!
-  useEffect(() => {
-    const loadRazorpay = () => {
-      const script = document.createElement("script");
-      script.src = "https://checkout.razorpay.com/v1/checkout.js";
-      script.async = true;
-      document.body.appendChild(script);
-    };
-    loadRazorpay();
-  }, []);
-
-  const handlePayment = () => {
-    const options = {
-      key: "rzp_test_0rLZuRany2Y32s", // Replace with your Razorpay Key ID
-      amount: 50000, // Amount in paise (50000 paise = 500 INR)
-      currency: "INR",
-      name: "Acme Corp",
-      description: "Test Transaction",
-      image: "https://example.com/your_logo",
-      order_id: "order_PwSBqGNMWcn0uc", // Replace with actual order ID
-      handler: function (response: any) {
-        alert("Payment ID: " + response.razorpay_payment_id);
-        alert("Order ID: " + response.razorpay_order_id);
-        alert("Signature: " + response.razorpay_signature);
-      },
-      prefill: {
-        name: "Gaurav Kumar",
-        email: "gaurav.kumar@example.com",
-        contact: "9000090000",
-      },
-      notes: {
-        address: "Razorpay Corporate Office",
-      },
-      theme: {
-        color: "#3399cc",
-      },
-    };
-    
-    const rzp1 = new (window as any).Razorpay(options);
-    rzp1.on("payment.failed", function (response: any) {
-      alert(response.error.code);
-      alert(response.error.description);
-      alert(response.error.source);
-      alert(response.error.step);
-      alert(response.error.reason);
-      alert(response.error.metadata.order_id);
-      alert(response.error.metadata.payment_id);
-    });
-    
-    rzp1.open();
-  };
-
   if (isLoading) {
     return (
       <div className="flex items-center gap-2">
@@ -100,7 +48,7 @@ const WalletBalance = ({
             {title ? (
               <p className="text-xs">{title}</p>
             ) : (
-              <p className="text-xs">Money check</p>
+              <p className="text-xs">Money</p>
             )}
             <span className="text-lg md:text-sm font-semibold">
               {balance?.toFixed(0)} &#8377;
@@ -109,7 +57,6 @@ const WalletBalance = ({
           <Button
             variant="ghost"
             className=" bg-green-600 hover:bg-green-700 text-white p-2 rounded-full"
-            onClick={handlePayment}
           >
             {buttonText ? buttonText : "+"}
           </Button>
